ca515d8a1f2701ea19daec1a77d559bda9a7ee77
Christian Stout <christian.allen.stout@gmail.com>
2024-10-07 18:55:30 -0600

    finally started parsing top_level

--- src/clank_parser.rs
+++ src/clank_parser.rs
@@ -16,22 +16,19 @@ impl ClankParser {
 
     pub fn build_tree_parse(&mut self, file: Pairs<'_, Rule>) {
         for top_level in file.into_iter() {
-            match top_level.as_rule() {
-                Rule::func => {
-                    println!("IN build_tree_parse=>func");
-                    let f = self.parse_fn(top_level.into_inner());
-                    self.tree.push(f);
-                }
-                Rule::r#struct => {
-                    println!("IN build_tree_parse=>struct");
-                    let f = self.parse_struct(top_level.into_inner());
-                    self.tree.push(f)
-                }
-                Rule::EOI => {
-                    println!("IN build_tree_parse=>EOI");
-                }
-                _ => {
-                    println!("IN build_tree_parse=>other");
+            for item in top_level.into_inner() {
+                match item.as_rule() {
+                    Rule::func => {
+                        let f = self.parse_fn(item.into_inner());
+                        self.tree.push(f);
+                    }
+                    Rule::r#struct => {
+                        let s = self.parse_struct(item.into_inner());
+                        self.tree.push(s);
+                    }
+                    _ => {
+                        unreachable!();
+                    }
                 }
             }
         }
@@ -64,49 +61,5 @@ pub fn parse_clank(input: String) -> Vec<TopLevel> {
     println!("{}", file.clone().into_inner());
     clank_parser.build_tree_parse(file.into_inner());
 
-    // for top_level in file.into_inner() {
-    //     match top_level.as_rule() {
-    //         Rule::func => {}
-    //         Rule::r#struct => {}
-    //         Rule::EOI => {}
-    //         _ => {}
-    //     }
-    // }
-
-    // clank_parser.build_tree_parse(file.into_inner());
-
     return clank_parser.tree;
-
-    // let mut i = 0;
-
-    // for top_level in file.into_inner() {
-    //     match top_level.as_rule() {
-    //         _ => {
-    //             println!("{}: {:?}", i, top_level);
-    //             i += 1;
-    //         }
-    //     }
-    // }
-
-    // println!("{:?}", file.into_inner())
-
-    // let mut field_sum: f64 = 0.0;
-    // let mut record_count: u64 = 0;
-
-    // for record in file.into_inner() {
-    //     match record.as_rule() {
-    //         Rule::program => {
-    //             record_count += 1;
-
-    //             for field in record.into_inner() {
-    //                 field_sum += field.as_str().parse::<f64>().unwrap();
-    //             }
-    //         }
-    //         Rule::EOI => (),
-    //         _ => unreachable!(),
-    //     }
-    // }
-
-    // println!("Sum of fields: {}", field_sum);
-    // println!("Number of records: {}", record_count);
 }
